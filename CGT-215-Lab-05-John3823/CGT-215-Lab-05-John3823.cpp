cefa5f6fe11f0c4e95685103c1f1d9968aba3646
Grant <matthewgrant.j@gmail.com>
2026-09-16 13:21:36 -0400

    added ccp file

---
+++ CGT-215-Lab-05-John3823/CGT-215-Lab-05-John3823.cpp
@@ -0,0 +1,10 @@
+// CGT-215-Lab-05-John3823.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
