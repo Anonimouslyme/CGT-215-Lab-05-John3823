fb9b4c2d6519f4d43c40512112c9873a12ba10f3
Grant <matthewgrant.j@gmail.com>
2026-09-16 13:58:43 -0400

    Code added

--- CGT-215-Lab-05-John3823/CGT-215-Lab-05-John3823.cpp
+++ CGT-215-Lab-05-John3823/CGT-215-Lab-05-John3823.cpp
@@ -2,9 +2,43 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string input;
+
+        vector<char> cipher = {
+            'V', 'F', 'X', 'B', 'L', 'I', 'T', 'Z', 'J',
+            'R', 'P', 'H', 'D', 'K', 'N', 'O', 'W', 'S',
+            'G', 'U', 'Y', 'Q', 'M', 'A', 'C', 'E'
+        };
+
+		cout << "Enter a string to encrypt: ";
+		getline(cin, input);
+
+		for (int i = 0; i < input.length(); i++)
+		{
+			char c = input[i];
+
+			if (c >= 65 && c <= 90)
+			{
+				cout << cipher[c - 65];
+			}
+			else if (c >= 97 && c <= 122)
+			{
+				
+				cout << char(cipher[c - 97] + 32);
+			}
+			else
+			{
+				cout << c;
+			}
+		}
+
 }
 
